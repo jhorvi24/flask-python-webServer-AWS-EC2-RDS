0ccd8fc2661a3f84a6af61a775923fab85900534
Jhorman Villanueva <jhorvi24@hotmail.com>
2024-07-24 15:57:54 -0500

    Configure the structure

--- app.py
+++ app.py
@@ -3,41 +3,53 @@ import mysql.connector
 import boto3
 
 app = Flask(__name__)
-ssm_client = boto3.client('ssm', region_name='us-east-1')
+# ssm_client = boto3.client('ssm', region_name='us-east-1')
 
-host='/book/host'
-user='/book/user'
-password='/book/password'
-database='/book/database'
+# host='/book/host'
+# user='/book/user'
+# password='/book/password'
+# database='/book/database'
 
-response = ssm_client.get_parameters(
-        Names=[host, user, password, database],
-        WithDecryption=True
-)
+# response = ssm_client.get_parameters(
+#         Names=[host, user, password, database],
+#         WithDecryption=True
+# )
 
-rds_database = response['Parameters'][0]['Value']
-rds_host = response['Parameters'][1]['Value']
-rds_password = response['Parameters'][2]['Value']
-rds_user = response['Parameters'][3]['Value']
+# rds_database = response['Parameters'][0]['Value']
+# rds_host = response['Parameters'][1]['Value']
+# rds_password = response['Parameters'][2]['Value']
+# rds_user = response['Parameters'][3]['Value']
 
 
 @app.route('/')
 def home():
 	return render_template("index.html")
 
-@app.route('/libros')
-def libros():
-    conn = mysql.connector.connect(
-        host=rds_host,
-        user=rds_user,
-        password=rds_password,
-        database=rds_database)
+@app.route('/books')
+def books():
+    # conn = mysql.connector.connect(
+    #     host=rds_host,
+    #     user=rds_user,
+    #     password=rds_password,
+    #     database=rds_database)
     
-    c = conn.cursor()
-    c.execute("SELECT * FROM Books")
-    books = c.fetchall()
-    conn.close()
+    # c = conn.cursor()
+    # c.execute("SELECT * FROM Books")
+    # books = c.fetchall()
+    # conn.close()
     return render_template("libros.html", books=books)
+
+@app.route('/about_us')
+def about_us():
+    return render_template("about.html")
+
+@app.route('/gallery')
+def gallery():
+    render_template("gallery.html")
+
+@app.route('/services')
+def services():
+    render_template("services.html")
 
 if __name__=='__main__':
 	app.run(host='0.0.0.0')
