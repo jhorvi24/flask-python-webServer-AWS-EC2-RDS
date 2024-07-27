7f80fb7d065c6f9ee8f0ad53d1133275a7bbb9cc
Jhorman Villanueva <jhorvi24@hotmail.com>
2024-07-27 15:38:17 -0500

    project ok for push to github

--- app.py
+++ app.py
@@ -3,22 +3,22 @@ import mysql.connector
 import boto3
 
 app = Flask(__name__)
-# ssm_client = boto3.client('ssm', region_name='us-east-1')
+ssm_client = boto3.client('ssm', region_name='us-east-1')
 
-# host='/book/host'
-# user='/book/user'
-# password='/book/password'
-# database='/book/database'
+host='/book/host'
+user='/book/user'
+password='/book/password'
+database='/book/database'
 
-# response = ssm_client.get_parameters(
-#         Names=[host, user, password, database],
-#         WithDecryption=True
-# )
+response = ssm_client.get_parameters(
+        Names=[host, user, password, database],
+        WithDecryption=True
+)
 
-# rds_database = response['Parameters'][0]['Value']
-# rds_host = response['Parameters'][1]['Value']
-# rds_password = response['Parameters'][2]['Value']
-# rds_user = response['Parameters'][3]['Value']
+rds_database = response['Parameters'][0]['Value']
+rds_host = response['Parameters'][1]['Value']
+rds_password = response['Parameters'][2]['Value']
+rds_user = response['Parameters'][3]['Value']
 
 
 @app.route('/')
@@ -27,17 +27,17 @@ def home():
 
 @app.route('/books')
 def books():
-    # conn = mysql.connector.connect(
-    #     host=rds_host,
-    #     user=rds_user,
-    #     password=rds_password,
-    #     database=rds_database)
+    conn = mysql.connector.connect(
+        host=rds_host,
+        user=rds_user,
+        password=rds_password,
+        database=rds_database)
     
-    # c = conn.cursor()
-    # c.execute("SELECT * FROM Books")
-    # books = c.fetchall()
-    # conn.close()
-    return render_template("libros.html", books=books)
+    c = conn.cursor()
+    c.execute("SELECT * FROM Books")
+    books = c.fetchall()
+    conn.close()
+    return render_template("books.html", books=books)
 
 @app.route('/about_us')
 def about_us():
